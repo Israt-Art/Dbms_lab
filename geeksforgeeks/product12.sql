a98b64886c564e3cf891a034203d50508dcf7f97
Israt-Art <ijshoknna2005@gmail.com>
2025-11-30 22:47:36 +0600

    Add files via upload

---
+++ geeksforgeeks/product12.sql
@@ -0,0 +1,22 @@
+create database geeksforgeeks13;
+use geeksforgeek13;
+
+
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    unit_price DECIMAL(10, 2)
+);
+
+-- Insert sample data into Products table
+
+INSERT INTO Products (product_id, product_name, category, unit_price) VALUES
+(101, 'Laptop', 'Electronics', 500.00),
+(102, 'Smartphone', 'Electronics', 300.00),
+(103, 'Headphones', 'Electronics', 30.00),
+(104, 'Keyboard', 'Electronics', 20.00),
+(105, 'Mouse', 'Electronics', 15.00);
+
+select product_name, unit_price from Products
+order by unit_price desc limit 2;
---
+++ geeksforgeeks/sale18.sql
@@ -0,0 +1,23 @@
+create database geeksforgeeks20;
+use geeksforgeeks20;
+
+CREATE TABLE Sales (
+    sale_id INT PRIMARY KEY,
+    product_id INT,
+    quantity_sold INT,
+    sale_date DATE,
+    total_price DECIMAL(10, 2)
+);
+
+
+
+INSERT INTO Sales (sale_id, product_id, quantity_sold, sale_date, total_price) VALUES
+(1, 101, 5, '2024-01-01', 2500.00),
+(2, 102, 3, '2024-01-02', 900.00),
+(3, 103, 2, '2024-01-02', 60.00),
+(4, 104, 4, '2024-01-03', 80.00),
+(5, 105, 6, '2024-01-03', 90.00);
+
+
+
+select sale_id,date_format(sale_date, '%Y-%m-%d') as formatted_date from Sales;
